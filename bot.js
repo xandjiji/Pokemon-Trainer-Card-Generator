cda47f73af83df16cbbede66fb43c49743273de3
xand <xandjiji@gmail.com>
2019-06-05 21:42:28 -0300

    code organization + better logging

--- bot.js
+++ bot.js
@@ -1,71 +1,15 @@
-var Twitter    = require('twitter');
-var Jimp       = require('jimp');
-var config     = require('./config');
+var Twitter     = require('twitter');
+var Jimp        = require('jimp');
+var config      = require('./config');
+var utils       = require('./utils');
 
 var client = new Twitter(config);
 
-var arroba = "@PokeTrainerCard";
+console.log(utils.timeStamp() + 'bot iniciando...');
 
-var timestamp = new Date().toLocaleTimeString('en-US', {hour12: false, hour: "numeric", minute: "numeric"});
-
-console.log('[' + timestamp + '] bot iniciado!');
-
-// arrays de cidade/regiao
-var regions = [
-	'Kanto',
-    'Johto'];
-var cities = [
-	'Celadon City',
-	'Azalea Town',
-	'Cerulean City',
-	'Blackthorn City',
-	'Cinnabar Island',
-	'Cherrygrove City',
-	'Fuchsia City',
-	'Cianwood City',
-	'Lavender Town',
-	'Ecruteak City',
-	'Pallet Town',
-	'Goldenrod City',
-	'Pewter City',
-	'Mahogany Town',
-	'Saffron City',
-	'New Bark Town',
-	'Vermilion City',
-	'Olivine City',
-	'Viridian City',
-	'Violet City'];
-
-var emojis = [':-)', '>:-)', '<3', ';-)', ':-D', ':-*', ':-O', ':-v', 'c-:', '~(^_^)~', '(n_n)', ':-P', ':^)'];
-
-
+// carrega os assets em jimps[]
 var jimps = [];
-
-// carregando cartao
-jimps.push(Jimp.read('assets/card.png'));
-
-// carregando pokemons
-for (var i = 1; i < 253; i++) {
-    jimps.push(Jimp.read('assets/pokemons/pokemon' + (i-1) + '.png'));
-}
-
-// carregando insigneas (kanto)
-for (var i = 253; i < 261; i++) {
-    jimps.push(Jimp.read('assets/badges/kanto/kanto' + (i-253) + '.png'));
-}
-
-// carregando insigneas (johto)
-for (var i = 261; i < 269; i++) {
-    jimps.push(Jimp.read('assets/badges/johto/johto' + (i-253) + '.png'));
-}
-
-// carregando treinadores
-for (var i = 269; i < 376; i++) {
-    jimps.push(Jimp.read('assets/trainers/trainer' + (i-269) + '.png'));
-}
-
-// salvando template
-jimps.push(Jimp.read('assets/card.png'));
+carregaAssets();
 
 // posicoes relativas grid de pokemons
 let pokegridW = 28;
@@ -79,295 +23,184 @@ let horizontal = 52;
 let offset = 48;
 let regionoffset = 0;
 
+// arrays e contadores
 let cooldownList = [];
 let tweetQueue = [];
 
 let successCount = 0;
 let failCount = 0;
 let retries = 0;
-
 let cooldownMinutes = 10;
 
+// reseta a lista de cooldown a cada 10 minutos
+setInterval(resetCooldown, cooldownMinutes * 1000 * 60);
 function resetCooldown() {
-	cooldownList = [];
+    cooldownList = [];
+    utils.resetMsg();
 }
 
-setInterval(resetCooldown, cooldownMinutes * 1000 * 60);
-//setInterval(reTweet, 1000 * 60);
+// retuita periodicamente todos os tuites que falharam (NAO TESTADO)
+/* setInterval(reTweet, 1000 * 60); */
 
+console.log(utils.timeStamp() + 'bot pronto!');
+// dispara ao capturar alguma mention
 client.stream('statuses/filter', {track: '@PokeTrainerCard'},  function(stream) {
     stream.on('data', function(tweet) {
+        
+        // caso o user nao esteja em cooldown
+        if(cooldownList.includes(tweet.user.name) == false) {
 
-	    let isOnCooldown = false;
+            var followcount = tweet.user.followers_count;
 
-		for(var i = 0; i < cooldownList.length; i++) {
-			if(tweet.user.name == cooldownList[i]) {
-				isOnCooldown = true;
-			}
-		}
+            var trainercard = utils.makeTrainercard(tweet.user.screen_name);
 
-        var tweetid = tweet.id;
-        var tweetidstr = tweet.id_str;
-        var tweetername = tweet.user.name;
-        var followcount = tweet.user.followers_count;
+            Promise.all(jimps).then(function(data) {
+                return Promise.all(jimps);
+            }).then(function(data) {
 
+                /*
+                    card:       0
+                    pokemons:   1-252
+                    badges:     253-268
+                    trainers:   269-375
+                    template:   376
+                */
 
-        var hash = sha256(tweet.user.screen_name);
+                // desenhando pokemon1
+                data[0].composite(data[1+trainercard.pokemon1], pokegridW, pokegridH);
 
-        var trainercard = [{
-            'id': '',
-            'name': '',
-            'region': '',
-            'hometown': '',
-            'money': '',
-            'pokedex': '',
-            'badges': '',
-            'trainer': '',
-            'pokemon1': '',
-            'pokemon2': '',
-            'pokemon3': '',
-            'pokemon4': '',
-            'pokemon5': '',
-            'pokemon6': ''}];
+                // desenhando pokemon2
+                data[0].composite(data[1+trainercard.pokemon2], pokegridW + (offsetW), pokegridH);
 
+                // desenhando pokemon3
+                data[0].composite(data[1+trainercard.pokemon3], pokegridW + (offsetW*2), pokegridH);
 
-        trainercard.name        =           tweet.user.screen_name;
-     	trainercard.region      =   regions[(parseInt('0x' + hash.substring(0, 4)) % 2)];
-     	trainercard.hometown    =   cities[	(parseInt('0x' + hash.substring(0, 4)) % 20)];
-     	trainercard.money       = 			(parseInt('0x' + hash.substring(5, 9)));
-     	trainercard.pokedex     = 			(parseInt('0x' + hash.substring(10, 14)) % 252);
-     	trainercard.badges      = 			(parseInt('0x' + hash.substring(15, 19)) % 9);
-     	trainercard.trainer		= 			(parseInt('0x' + hash.substring(20, 24)) % 107);
-     	trainercard.pokemon1	= 			(parseInt('0x' + hash.substring(25, 29)) % 252);
-     	trainercard.pokemon2	= 			(parseInt('0x' + hash.substring(30, 34)) % 252);
-     	trainercard.pokemon3	= 			(parseInt('0x' + hash.substring(35, 39)) % 252);
-     	trainercard.pokemon4	= 			(parseInt('0x' + hash.substring(40, 44)) % 252);
-     	trainercard.pokemon5	= 			(parseInt('0x' + hash.substring(45, 49)) % 252);
-     	trainercard.pokemon6    = 			(parseInt('0x' + hash.substring(50, 54)) % 252);
-     	trainercard.id          =           (parseInt('0x' + hash.substring(55, 59)));
+                // desenhando pokemon4
+                data[0].composite(data[1+trainercard.pokemon4], pokegridW, pokegridH + (offsetH));
 
-        Promise.all(jimps).then(function(data) {
-            return Promise.all(jimps);
-        }).then(function(data) {
+                // desenhando pokemon5
+                data[0].composite(data[1+trainercard.pokemon5], pokegridW + (offsetW), pokegridH + (offsetH));
 
-            /*
-                card:     0
-                pokemons: 1-252
-                badges:   253-268
-                trainers: 269-375
-                template:    376
-            */
-
-            // desenhando pokemon1
-            data[0].composite(data[1+trainercard.pokemon1], pokegridW, pokegridH);
-
-            // desenhando pokemon2
-            data[0].composite(data[1+trainercard.pokemon2], pokegridW + (offsetW), pokegridH);
-
-            // desenhando pokemon3
-            data[0].composite(data[1+trainercard.pokemon3], pokegridW + (offsetW*2), pokegridH);
-
-            // desenhando pokemon4
-            data[0].composite(data[1+trainercard.pokemon4], pokegridW, pokegridH + (offsetH));
-
-            // desenhando pokemon5
-            data[0].composite(data[1+trainercard.pokemon5], pokegridW + (offsetW), pokegridH + (offsetH));
-
-            // desenhando pokemon6
-            data[0].composite(data[1+trainercard.pokemon6], pokegridW + (offsetW*2), pokegridH + (offsetH));
+                // desenhando pokemon6
+                data[0].composite(data[1+trainercard.pokemon6], pokegridW + (offsetW*2), pokegridH + (offsetH));
 
 
-            // desenhando treinador
-            data[0].composite(data[269+trainercard.trainer], 260, 170);
+                // desenhando treinador
+                data[0].composite(data[269+trainercard.trainer], 260, 170);
 
-            // desenhando insigneas
-            if(trainercard.region == 'Johto') {
-                regionoffset = 8;
-            }
-            if(trainercard.region == 'Kanto') {
-                regionoffset = 0;
-            }
-            for(var i = 0; i < trainercard.badges; i++) {
-                data[0].composite(data[253+i+regionoffset], horizontal + (i*offset), 380);
-            }
+                // desenhando insigneas
+                if(trainercard.region == 'Johto') {
+                    regionoffset = 8;
+                }
+                if(trainercard.region == 'Kanto') {
+                    regionoffset = 0;
+                }
+                for(var i = 0; i < trainercard.badges; i++) {
+                    data[0].composite(data[253+i+regionoffset], horizontal + (i*offset), 380);
+                }
 
-            // escrevendo
-            Jimp.loadFont('assets/pokedex.fnt').then(font => {
+                // escrevendo no cartao
+                Jimp.loadFont('assets/pokedex.fnt').then(font => {
 
-                // -12
-                let card = data[0];
+                    // -12
+                    let card = data[0];
 
-                card.print(font, 263, 30,   'IDNo. ' + trainercard.id);
-                card.print(font, 45, 72,    'NAME: ' + trainercard.name);
-                card.print(font, 45, 126,   trainercard.hometown + '  (' + trainercard.region + ')');
-                card.print(font, 45, 158,   'MONEY: $' + trainercard.money);
-                card.print(font, 45, 190,   'POKéDEX: ' + trainercard.pokedex);
+                    card.print(font, 263, 30,   'IDNo. ' + trainercard.id);
+                    card.print(font, 45, 72,    'NAME: ' + trainercard.name);
+                    card.print(font, 45, 126,   trainercard.hometown + '  (' + trainercard.region + ')');
+                    card.print(font, 45, 158,   'MONEY: $' + trainercard.money);
+                    card.print(font, 45, 190,   'POKéDEX: ' + trainercard.pokedex);
 
-                card.write('saved/' + trainercard.name + '.png', function() {
-                    
-                    data[0].composite(data[376], 0, 0);
+                    // salvando cartao
+                    card.write('saved/' + trainercard.name + '.png', function() {
+                        
+                        // resetando data[0]
+                        data[0].composite(data[376], 0, 0);
 
-                    var imagem = require('fs').readFileSync('saved/' + trainercard.name + '.png');
+                        // upando cartao
+                        var imagem = require('fs').readFileSync('saved/' + trainercard.name + '.png');
 
-                    client.post('media/upload', {media: imagem}, function(error, imagem, response) {
+                        client.post('media/upload', {media: imagem}, function(error, imagem, response) {
+                            if (!error) {
 
-                        var timestamp = new Date().toLocaleTimeString('en-US', {hour12: false, hour: "numeric", minute: "numeric"});
+                                //tuitando
+                                var status = utils.makeTweet(tweet, imagem);
 
-                        if (!error && isOnCooldown == false) {
-
-                            //tuitando
-                            cooldownList.push(tweet.user.name);
-                            var status = {
-                                status: '@' + trainercard.name + ' here you go, ' + tweetername + '!',
-                                in_reply_to_status_id: tweetidstr,
-                                media_ids: imagem.media_id_string
+                                client.post('statuses/update', status, function(error, tweeted, response) {
+                                    if (!error) {
+                                        successCount++;
+                                        utils.successMsg(tweet, successCount);
+                                        cooldownList.push(tweet.user.name);
+                                    }
+                                    if (error) {
+                                        failCount++;
+                                        utils.failMsg(tweet, failCount);
+                                        console.log(error);
+                                        tweetQueue.push(status);
+                                    }
+                                });
                             }
-
-                            client.post('statuses/update', status, function(error, tweet, response) {
-                                if (!error) {
-                                    successCount++;
-                                    //console.log(tweet);
-
-                                    //console.log('respondido ao ID: ' + tweetid);
-                                    console.log('[' + timestamp + '] @' + trainercard.name + ' finalizado (' + followcount + ' seguidores) (#' + successCount + ')');
-                                }
-                                if (error) {
-                                    failCount++;
-                                    console.log(error);
-                                    console.log('[' + timestamp + '] @' + trainercard.name + ' falhou (' + followcount + ' seguidores) (#' + failCount + ')');
-                                    data[0].composite(data[376], 0, 0);
-                                    tweetQueue.push(status);
-                                }
-                            });
-                        }
-                        if(!error && isOnCooldown == true) {
-                            console.log('-> [' + timestamp + '] @' + trainercard.name + ' bloqueado pelo cooldown');
-                        }
+                            if(error) {
+                                failCount++;
+                                utils.failMsg(tweet, failCount);
+                                console.log(error);
+                            }
+                        });
                     });
                 });
-            });
-        })
-
-
-          //tuita('@' + tweet.user.screen_name + ' here you go!');
+            })
+        } else {
+            // caso o user esteja em cooldown
+            utils.cooldownMsg(tweet);
+        }
     });
-
     stream.on('error', function(error) {
+        failCount++;
         console.log(error);
-        var timestamp = new Date().toLocaleTimeString('en-US', {hour12: false, hour: "numeric", minute: "numeric"});
-        console.log('[' + timestamp + '] falhou');
     });
 });
 
-var sha256 = function sha256(ascii) {
-	function rightRotate(value, amount) {
-		return (value>>>amount) | (value<<(32 - amount));
-	};
+// carrega os assets
+function carregaAssets() {
 
-	var mathPow = Math.pow;
-	var maxWord = mathPow(2, 32);
-	var lengthProperty = 'length'
-	var i, j; // Used as a counter across the whole file
-	var result = ''
+    console.log(utils.timeStamp() + 'carregando os assets...');
 
-	var words = [];
-	var asciiBitLength = ascii[lengthProperty]*8;
+    // carregando cartao
+    jimps.push(Jimp.read('assets/card.png'));
 
-	//* caching results is optional - remove/add slash from front of this line to toggle
-	// Initial hash value: first 32 bits of the fractional parts of the square roots of the first 8 primes
-	// (we actually calculate the first 64, but extra values are just ignored)
-	var hash = sha256.h = sha256.h || [];
-	// Round constants: first 32 bits of the fractional parts of the cube roots of the first 64 primes
-	var k = sha256.k = sha256.k || [];
-	var primeCounter = k[lengthProperty];
-	/*/
-	var hash = [], k = [];
-	var primeCounter = 0;
-	//*/
+    // carregando pokemons
+    for(var i = 1; i < 253; i++) {
+        jimps.push(Jimp.read('assets/pokemons/pokemon' + (i-1) + '.png'));
+    }
 
-	var isComposite = {};
-	for (var candidate = 2; primeCounter < 64; candidate++) {
-		if (!isComposite[candidate]) {
-			for (i = 0; i < 313; i += candidate) {
-				isComposite[i] = candidate;
-			}
-			hash[primeCounter] = (mathPow(candidate, .5)*maxWord)|0;
-			k[primeCounter++] = (mathPow(candidate, 1/3)*maxWord)|0;
-		}
-	}
+    // carregando insigneas (kanto)
+    for(var i = 253; i < 261; i++) {
+        jimps.push(Jimp.read('assets/badges/kanto/kanto' + (i-253) + '.png'));
+    }
 
-	ascii += '\x80' // Append Ƈ' bit (plus zero padding)
-	while (ascii[lengthProperty]%64 - 56) ascii += '\x00' // More zero padding
-	for (i = 0; i < ascii[lengthProperty]; i++) {
-		j = ascii.charCodeAt(i);
-		if (j>>8) return; // ASCII check: only accept characters in range 0-255
-		words[i>>2] |= j << ((3 - i)%4)*8;
-	}
-	words[words[lengthProperty]] = ((asciiBitLength/maxWord)|0);
-	words[words[lengthProperty]] = (asciiBitLength)
+    // carregando insigneas (johto)
+    for(var i = 261; i < 269; i++) {
+        jimps.push(Jimp.read('assets/badges/johto/johto' + (i-253) + '.png'));
+    }
 
-	// process each chunk
-	for (j = 0; j < words[lengthProperty];) {
-		var w = words.slice(j, j += 16); // The message is expanded into 64 words as part of the iteration
-		var oldHash = hash;
-		// This is now the undefinedworking hash", often labelled as variables a...g
-		// (we have to truncate as well, otherwise extra entries at the end accumulate
-		hash = hash.slice(0, 8);
+    // carregando treinadores
+    for(var i = 269; i < 376; i++) {
+        jimps.push(Jimp.read('assets/trainers/trainer' + (i-269) + '.png'));
+    }
 
-		for (i = 0; i < 64; i++) {
-			var i2 = i + j;
-			// Expand the message into 64 words
-			// Used below if
-			var w15 = w[i - 15], w2 = w[i - 2];
+    // salvando template
+    jimps.push(Jimp.read('assets/card.png'));
+}
 
-			// Iterate
-			var a = hash[0], e = hash[4];
-			var temp1 = hash[7]
-				+ (rightRotate(e, 6) ^ rightRotate(e, 11) ^ rightRotate(e, 25)) // S1
-				+ ((e&hash[5])^((~e)&hash[6])) // ch
-				+ k[i]
-				// Expand the message schedule if needed
-				+ (w[i] = (i < 16) ? w[i] : (
-						w[i - 16]
-						+ (rightRotate(w15, 7) ^ rightRotate(w15, 18) ^ (w15>>>3)) // s0
-						+ w[i - 7]
-						+ (rightRotate(w2, 17) ^ rightRotate(w2, 19) ^ (w2>>>10)) // s1
-					)|0
-				);
-			// This is only used once, so *could* be moved below, but it only saves 4 bytes and makes things unreadble
-			var temp2 = (rightRotate(a, 2) ^ rightRotate(a, 13) ^ rightRotate(a, 22)) // S0
-				+ ((a&hash[1])^(a&hash[2])^(hash[1]&hash[2])); // maj
-
-			hash = [(temp1 + temp2)|0].concat(hash); // We don't bother trimming off the extra ones, they're harmless as long as we're truncating when we do the slice()
-			hash[4] = (hash[4] + temp1)|0;
-		}
-
-		for (i = 0; i < 8; i++) {
-			hash[i] = (hash[i] + oldHash[i])|0;
-		}
-	}
-
-
-	for (i = 0; i < 8; i++) {
-		for (j = 3; j + 1; j--) {
-			var b = (hash[i]>>(j*8))&255;
-			result += ((b < 16) ? 0 : '') + b.toString(16);
-		}
-	}
-
-	return result;
-};
-
+// retuita um tuite que falhou que falharam (NAO TESTADO)
 function reTweet() {
 
 	if(tweetQueue.length != 0) {
 		client.post('statuses/update', tweetQueue[0], function(error, tweet, response) {
 			if (!error) {
 
-				var timestamp2 = new Date().toLocaleTimeString('en-US', {hour12: false, hour: "numeric", minute: "numeric"});
-
 				successCount++;
-				console.log('[' + timestamp2 + '] tweet da fila feito com sucesso! Restam: ' + tweetQueue.length + ' (#' + successCount + ')');
+				console.log(utils.timeStamp() + ' tweet da fila feito com sucesso! Restam: ' + tweetQueue.length + ' (#' + successCount + ')');
 				tweetQueue.shift();
 
             }
---
+++ utils.js
@@ -0,0 +1,228 @@
+var utils = {}
+
+/* arrays de cidade/regiao/emoji */
+
+utils.regions = [
+	'Kanto',
+    'Johto'];
+
+utils.cities = [
+	'Celadon City',
+	'Azalea Town',
+	'Cerulean City',
+	'Blackthorn City',
+	'Cinnabar Island',
+	'Cherrygrove City',
+	'Fuchsia City',
+	'Cianwood City',
+	'Lavender Town',
+	'Ecruteak City',
+	'Pallet Town',
+	'Goldenrod City',
+	'Pewter City',
+	'Mahogany Town',
+	'Saffron City',
+	'New Bark Town',
+	'Vermilion City',
+	'Olivine City',
+	'Viridian City',
+	'Violet City'];
+
+utils.emojis = [
+    ':-)',
+    '>:-)',
+    '<3',
+    ';-)',
+    ':-D',
+    ':-*',
+    ':-O',
+    ':-v',
+    'c-:',
+    '~(^_^)~',
+    '(n_n)',
+    ':-P',
+    ':^)'];
+
+/* funcoes de console.log() */
+
+utils.timeStamp = function timeStamp() {
+    var time = new Date().toLocaleTimeString('en-US', {hour12: false, hour: "numeric", minute: "numeric", second: "numeric"});
+    return '[' + time + '] ';
+}
+
+utils.successMsg = function successMsg(tweet, successCount) {
+    
+    var msg =   utils.timeStamp() +
+                '@' + tweet.user.screen_name + ' finalizado ' +
+                '(' + tweet.user.followers_count + ' seguidores) ' + 
+                '(#' + successCount + ')';
+
+    return console.log(msg);
+}
+
+utils.failMsg = function failMsg(tweet, failCount) {
+    
+    var msg =   utils.timeStamp() +
+                '@' + tweet.user.screen_name + ' falhou ' +
+                '(' + tweet.user.followers_count + ' seguidores) ' +
+                '(#' + failCount + ')';
+
+    return console.log(msg);
+}
+
+utils.cooldownMsg = function cooldownMsg(tweet) {
+
+    var msg =   '-> ' + utils.timeStamp() +
+                '@' + tweet.user.screen_name + ' bloqueado pelo cooldown';
+
+    return console.log(msg);
+}
+
+utils.resetMsg = function resetMsg() {
+
+    var msg =   utils.timeStamp() +
+                'lista de cooldown resetada';
+
+    return console.log(msg);    
+}
+
+/* funcoes de montar tuite e trainercard */
+
+utils.makeTrainercard = function makeTrainercard(screen_name) {
+
+    var hash = utils.sha256(screen_name);
+
+    var trainercard = {
+        name:       screen_name,
+        region:     utils.regions[  (parseInt('0x' + hash.substring(0, 4)) % 2)],
+        hometown:   utils.cities[   (parseInt('0x' + hash.substring(0, 4)) % 20)],
+        money:                      (parseInt('0x' + hash.substring(5, 9))),
+        pokedex:                    (parseInt('0x' + hash.substring(10, 14)) % 252),
+        badges:                     (parseInt('0x' + hash.substring(15, 19)) % 9),
+        trainer:                    (parseInt('0x' + hash.substring(20, 24)) % 107),
+        pokemon1:                   (parseInt('0x' + hash.substring(25, 29)) % 252),
+        pokemon2:                   (parseInt('0x' + hash.substring(30, 34)) % 252),
+        pokemon3:                   (parseInt('0x' + hash.substring(35, 39)) % 252),
+        pokemon4:                   (parseInt('0x' + hash.substring(40, 44)) % 252),
+        pokemon5:                   (parseInt('0x' + hash.substring(45, 49)) % 252),
+        pokemon6:                   (parseInt('0x' + hash.substring(50, 54)) % 252),
+        id:                         (parseInt('0x' + hash.substring(55, 59))) 
+    };
+
+    return trainercard;
+}
+
+utils.makeTweet = function makeTweet(tweet, imagem) {
+
+    var composedTweet = {
+        status: '@' + tweet.user.screen_name + ' here you go, ' + tweet.user.name + '!',
+        in_reply_to_status_id: tweet.id_str,
+        media_ids: imagem.media_id_string
+    };
+
+    return composedTweet;
+
+}
+
+/* implementacao de sha256 */
+
+utils.sha256 = function sha256(ascii) {
+    function rightRotate(value, amount) {
+        return (value>>>amount) | (value<<(32 - amount));
+    };
+
+    var mathPow = Math.pow;
+    var maxWord = mathPow(2, 32);
+    var lengthProperty = 'length'
+    var i, j; // Used as a counter across the whole file
+    var result = ''
+
+    var words = [];
+    var asciiBitLength = ascii[lengthProperty]*8;
+
+    //* caching results is optional - remove/add slash from front of this line to toggle
+    // Initial hash value: first 32 bits of the fractional parts of the square roots of the first 8 primes
+    // (we actually calculate the first 64, but extra values are just ignored)
+    var hash = sha256.h = sha256.h || [];
+    // Round constants: first 32 bits of the fractional parts of the cube roots of the first 64 primes
+    var k = sha256.k = sha256.k || [];
+    var primeCounter = k[lengthProperty];
+    /*/
+    var hash = [], k = [];
+    var primeCounter = 0;
+    //*/
+
+    var isComposite = {};
+    for (var candidate = 2; primeCounter < 64; candidate++) {
+        if (!isComposite[candidate]) {
+            for (i = 0; i < 313; i += candidate) {
+                isComposite[i] = candidate;
+            }
+            hash[primeCounter] = (mathPow(candidate, .5)*maxWord)|0;
+            k[primeCounter++] = (mathPow(candidate, 1/3)*maxWord)|0;
+        }
+    }
+
+    ascii += '\x80' // Append Ƈ' bit (plus zero padding)
+    while (ascii[lengthProperty]%64 - 56) ascii += '\x00' // More zero padding
+    for (i = 0; i < ascii[lengthProperty]; i++) {
+        j = ascii.charCodeAt(i);
+        if (j>>8) return; // ASCII check: only accept characters in range 0-255
+        words[i>>2] |= j << ((3 - i)%4)*8;
+    }
+    words[words[lengthProperty]] = ((asciiBitLength/maxWord)|0);
+    words[words[lengthProperty]] = (asciiBitLength)
+
+    // process each chunk
+    for (j = 0; j < words[lengthProperty];) {
+        var w = words.slice(j, j += 16); // The message is expanded into 64 words as part of the iteration
+        var oldHash = hash;
+        // This is now the undefinedworking hash", often labelled as variables a...g
+        // (we have to truncate as well, otherwise extra entries at the end accumulate
+        hash = hash.slice(0, 8);
+
+        for (i = 0; i < 64; i++) {
+            var i2 = i + j;
+            // Expand the message into 64 words
+            // Used below if
+            var w15 = w[i - 15], w2 = w[i - 2];
+
+            // Iterate
+            var a = hash[0], e = hash[4];
+            var temp1 = hash[7]
+                + (rightRotate(e, 6) ^ rightRotate(e, 11) ^ rightRotate(e, 25)) // S1
+                + ((e&hash[5])^((~e)&hash[6])) // ch
+                + k[i]
+                // Expand the message schedule if needed
+                + (w[i] = (i < 16) ? w[i] : (
+                        w[i - 16]
+                        + (rightRotate(w15, 7) ^ rightRotate(w15, 18) ^ (w15>>>3)) // s0
+                        + w[i - 7]
+                        + (rightRotate(w2, 17) ^ rightRotate(w2, 19) ^ (w2>>>10)) // s1
+                    )|0
+                );
+            // This is only used once, so *could* be moved below, but it only saves 4 bytes and makes things unreadble
+            var temp2 = (rightRotate(a, 2) ^ rightRotate(a, 13) ^ rightRotate(a, 22)) // S0
+                + ((a&hash[1])^(a&hash[2])^(hash[1]&hash[2])); // maj
+
+            hash = [(temp1 + temp2)|0].concat(hash); // We don't bother trimming off the extra ones, they're harmless as long as we're truncating when we do the slice()
+            hash[4] = (hash[4] + temp1)|0;
+        }
+
+        for (i = 0; i < 8; i++) {
+            hash[i] = (hash[i] + oldHash[i])|0;
+        }
+    }
+
+
+    for (i = 0; i < 8; i++) {
+        for (j = 3; j + 1; j--) {
+            var b = (hash[i]>>(j*8))&255;
+            result += ((b < 16) ? 0 : '') + b.toString(16);
+        }
+    }
+
+    return result;
+}
+
+module.exports = utils;
