e75a3de9043793773e702ce66521d1cc988e31cb
xand <xandjiji@gmail.com>
2019-06-11 16:52:56 -0300

    color definitions

--- logging.js
+++ logging.js
@@ -1,14 +1,13 @@
 var logging = {}
 
 logging.colors = {
-    reset:      '\x1b[0m',
-    red:        '\x1b[31m',
-    green:      '\x1b[32m',
-    yellow:     '\x1b[33m',
-    blue:       '\x1b[34m',
-    magenta:    '\x1b[35m',
-    cian:       '\x1b[36m',
-    gray:       '\x1b[90m'
+    reset:      '\x1b[0m',  // white
+    fail:       '\x1b[31m', // red
+    success:    '\x1b[32m', // green
+    followers:  '\x1b[33m', // yellow
+    system:     '\x1b[35m', // magenta
+    screenName: '\x1b[36m', // cian
+    control:    '\x1b[90m'  // gray
 }
 
 logging.numberFormat = function numberFormat(x) {
@@ -23,7 +22,7 @@ logging.timeStamp = function timeStamp() {
 logging.startupMsg = function startupMsg() {
     
     let msg =
-        `${logging.colors.magenta}${logging.timeStamp()}` +
+        `${logging.colors.system}${logging.timeStamp()}` +
         `${logging.colors.reset}bot is starting...${logging.colors.reset}`;
 
     return console.log(msg);
@@ -32,7 +31,7 @@ logging.startupMsg = function startupMsg() {
 logging.loadingAssetsMsg = function loadingAssetsMsg() {
     
     let msg =
-        `${logging.colors.magenta}${logging.timeStamp()}` +
+        `${logging.colors.system}${logging.timeStamp()}` +
         `${logging.colors.reset}loading assets...${logging.colors.reset}`;
 
     return console.log(msg);
@@ -41,8 +40,8 @@ logging.loadingAssetsMsg = function loadingAssetsMsg() {
 logging.initializedFileMsg = function initializedFileMsg() {
     
     let msg =
-        `${logging.colors.magenta}${logging.timeStamp()}` +
-        `${logging.colors.reset}the ${logging.colors.magenta}failedTweets.json${logging.colors.reset} file was created and initialized correctly!${logging.colors.reset}`;
+        `${logging.colors.system}${logging.timeStamp()}` +
+        `${logging.colors.reset}the ${logging.colors.system}failedTweets.json${logging.colors.reset} file was created and initialized correctly!${logging.colors.reset}`;
 
     return console.log(msg);
 }
@@ -50,8 +49,8 @@ logging.initializedFileMsg = function initializedFileMsg() {
 logging.zeroTweetsLoadedMsg = function zeroTweetsLoaded() {
     
     let msg =
-        `${logging.colors.magenta}${logging.timeStamp()}` +
-        `${logging.colors.reset}the tweet queue was loaded from the ${logging.colors.magenta}failedTweets.json${logging.colors.reset} file. ${logging.colors.magenta}[0]${logging.colors.reset}`;
+        `${logging.colors.system}${logging.timeStamp()}` +
+        `${logging.colors.reset}the tweet queue was loaded from the ${logging.colors.system}failedTweets.json${logging.colors.reset} file. ${logging.colors.system}[0]${logging.colors.reset}`;
 
     return console.log(msg);
 }
@@ -59,8 +58,8 @@ logging.zeroTweetsLoadedMsg = function zeroTweetsLoaded() {
 logging.tweetsLoadedMsg = function zeroTweetsLoaded(size) {
     
     let msg =
-        `${logging.colors.magenta}${logging.timeStamp()}` +
-        `${logging.colors.reset}the tweet queue was loaded from the ${logging.colors.magenta}failedTweets.json${logging.colors.reset} file. ${logging.colors.magenta}[${size}]${logging.colors.reset}`;
+        `${logging.colors.system}${logging.timeStamp()}` +
+        `${logging.colors.reset}the tweet queue was loaded from the ${logging.colors.system}failedTweets.json${logging.colors.reset} file. ${logging.colors.system}[${size}]${logging.colors.reset}`;
 
     return console.log(msg);
 }
@@ -68,7 +67,7 @@ logging.tweetsLoadedMsg = function zeroTweetsLoaded(size) {
 logging.readyMsg = function readyMsg() {
     
     let msg =
-        `${logging.colors.magenta}${logging.timeStamp()}` +
+        `${logging.colors.system}${logging.timeStamp()}` +
         `${logging.colors.reset}bot is ready!${logging.colors.reset}`;
 
     return console.log(msg);
@@ -77,7 +76,7 @@ logging.readyMsg = function readyMsg() {
 logging.errorMsg = function errorMsg(error) {
 
     let msg =
-        `${logging.colors.red}${logging.timeStamp()}${logging.colors.reset}` +
+        `${logging.colors.fail}${logging.timeStamp()}${logging.colors.reset}` +
         `┕> ${error[0].message}`;
 
     return console.log(msg);
@@ -86,10 +85,10 @@ logging.errorMsg = function errorMsg(error) {
 logging.successMsg = function successMsg(tweet, successCount) {
     
     let msg =
-        `${logging.colors.green}${logging.timeStamp()}` +
-        `${logging.colors.cian}@${tweet.user.screen_name}${logging.colors.reset} was successful. ` +
-        `${logging.colors.yellow}[${logging.numberFormat(tweet.user.followers_count)} followers]` +
-        `${logging.colors.gray}[#${successCount}]${logging.colors.reset}`;
+        `${logging.colors.success}${logging.timeStamp()}` +
+        `${logging.colors.screenName}@${tweet.user.screen_name}${logging.colors.reset} was successful. ` +
+        `${logging.colors.followers}[${logging.numberFormat(tweet.user.followers_count)} followers]` +
+        `${logging.colors.control}[#${successCount}]${logging.colors.reset}`;
 
     return console.log(msg);
 }
@@ -97,10 +96,10 @@ logging.successMsg = function successMsg(tweet, successCount) {
 logging.failMsg = function failMsg(tweet, failCount) {
     
     let msg =
-        `${logging.colors.red}${logging.timeStamp()}` +
-        `${logging.colors.cian}@${tweet.user.screen_name}${logging.colors.reset} has failed. ` +
-        `${logging.colors.yellow}[${logging.numberFormat(tweet.user.followers_count)} followers]` +
-        `${logging.colors.gray}[#${failCount}]${logging.colors.reset}`;
+        `${logging.colors.fail}${logging.timeStamp()}` +
+        `${logging.colors.screenName}@${tweet.user.screen_name}${logging.colors.reset} has failed. ` +
+        `${logging.colors.followers}[${logging.numberFormat(tweet.user.followers_count)} followers]` +
+        `${logging.colors.control}[#${failCount}]${logging.colors.reset}`;
 
     return console.log(msg);
 }
@@ -108,10 +107,10 @@ logging.failMsg = function failMsg(tweet, failCount) {
 logging.oldSuccessMsg = function oldSuccessMsg(queueSize, successCount) {
     
     let msg =
-        `${logging.colors.green}${logging.timeStamp()}` +
-        `${logging.colors.reset}a tweet from the ${logging.colors.magenta}failedTweets.json${logging.colors.reset} file was successful. ` +
-        `${logging.colors.magenta}[${queueSize} left]` +
-        `${logging.colors.gray}[#${successCount}]${logging.colors.reset}`;
+        `${logging.colors.success}${logging.timeStamp()}` +
+        `${logging.colors.reset}a tweet from the ${logging.colors.system}failedTweets.json${logging.colors.reset} file was successful. ` +
+        `${logging.colors.system}[${queueSize} left]` +
+        `${logging.colors.control}[#${successCount}]${logging.colors.reset}`;
 
     return console.log(msg);
 }
@@ -119,10 +118,10 @@ logging.oldSuccessMsg = function oldSuccessMsg(queueSize, successCount) {
 logging.oldFailMsg = function oldFailMsg(queueSize, retries) {
     
     let msg =
-        `${logging.colors.red}${logging.timeStamp()}` +
-        `${logging.colors.reset}a tweet from the ${logging.colors.magenta}failedTweets.json${logging.colors.reset} file has failed. ` +
-        `${logging.colors.magenta}[${queueSize} left]` +
-        `${logging.colors.gray}[#${retries}]${logging.colors.reset}`;
+        `${logging.colors.fail}${logging.timeStamp()}` +
+        `${logging.colors.reset}a tweet from the ${logging.colors.system}failedTweets.json${logging.colors.reset} file has failed. ` +
+        `${logging.colors.system}[${queueSize} left]` +
+        `${logging.colors.control}[#${retries}]${logging.colors.reset}`;
 
     return console.log(msg);
 }
@@ -130,8 +129,8 @@ logging.oldFailMsg = function oldFailMsg(queueSize, retries) {
 logging.cooldownMsg = function cooldownMsg(tweet) {
 
     let msg =
-        `${logging.colors.gray}${logging.timeStamp()}` +
-        `${logging.colors.cian}@${tweet.user.screen_name}${logging.colors.reset} is on cooldown!${logging.colors.reset}`;
+        `${logging.colors.control}${logging.timeStamp()}` +
+        `${logging.colors.screenName}@${tweet.user.screen_name}${logging.colors.reset} is on cooldown!${logging.colors.reset}`;
 
     return console.log(msg);
 }
@@ -139,7 +138,7 @@ logging.cooldownMsg = function cooldownMsg(tweet) {
 logging.resetMsg = function resetMsg() {
 
     let msg =
-        `${logging.colors.gray}${logging.timeStamp()}${logging.colors.reset}` +
+        `${logging.colors.control}${logging.timeStamp()}${logging.colors.reset}` +
         `the cooldown list was reset.${logging.colors.reset}`;
 
     return console.log(msg);    
