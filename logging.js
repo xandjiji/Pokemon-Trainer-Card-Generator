b273c4a3153045311a02c4a85fe7bdddcf92251a
xand <xandjiji@gmail.com>
2019-06-25 18:14:38 -0300

    bugfix again

--- logging.js
+++ logging.js
@@ -77,7 +77,7 @@ logging.errorMsg = function errorMsg(error) {
 
     let msg =
         `${logging.colors.fail}${logging.timeStamp()}${logging.colors.reset} ` +
-        `${logging.colors.fail}[${error[0].code}]${logging.colors.reset}`;
+        `${logging.colors.fail}[${error}]${logging.colors.reset}`;
 
     return console.log(msg);
 }
@@ -100,7 +100,7 @@ logging.failMsg = function failMsg(tweet, failCount, error) {
         `${logging.colors.screenName}@${tweet.user.screen_name}${logging.colors.reset} has failed. ` +
         `${logging.colors.followers}[${logging.numberFormat(tweet.user.followers_count)} followers]` +
         `${logging.colors.control}[#${failCount}]${logging.colors.reset}` +
-        `${logging.colors.fail}[${error[0].code}]${logging.colors.reset}`;
+        `${logging.colors.fail}[${error}]${logging.colors.reset}`;
 
     return console.log(msg);
 }
@@ -123,7 +123,7 @@ logging.oldFailMsg = function oldFailMsg(queueSize, retries, error) {
         `${logging.colors.reset}a tweet from the ${logging.colors.system}failedTweets.json${logging.colors.reset} file has failed. ` +
         `${logging.colors.system}[${queueSize} left]` +
         `${logging.colors.control}[#${retries}]${logging.colors.reset}` +
-        `${logging.colors.fail}[${error[0].code}]${logging.colors.reset}`;
+        `${logging.colors.fail}[${error}]${logging.colors.reset}`;
 
     return console.log(msg);
 }
