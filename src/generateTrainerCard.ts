a3f980295af1072aba18e6cc26364e512b075208
xandjiji <xandjiji@gmail.com>
2023-03-15 17:13:19 -0300

    feature: loading assets

---
+++ src/generateTrainerCard.ts
@@ -0,0 +1,37 @@
+import jimp from "jimp";
+import { COUNT, generateTrainerData } from "./generateTrainerData";
+
+const loadAll = (resolver: (idx: number) => string, length: number) =>
+  Promise.all(Array.from({ length }, (_, idx) => jimp.read(resolver(idx))));
+
+const assetLoader = async () => {
+  console.log("loading assets...");
+  const [pokemons, kantoBadges, johtoBadges, trainers] = await Promise.all([
+    loadAll((idx) => `assets/pokemons/${idx}.png`, COUNT.pokemons),
+    loadAll((idx) => `assets/badges/kanto/${idx}.png`, COUNT.badges),
+    loadAll((idx) => `assets/badges/johto/${idx}.png`, COUNT.badges),
+    loadAll((idx) => `assets/trainers/${idx}.png`, COUNT.trainers),
+  ]);
+
+  const badges = [...kantoBadges, ...johtoBadges];
+
+  return {
+    pokemon: (idx: number) => pokemons[idx],
+    badge: (idx: number) => badges[idx],
+    trainer: (idx: number) => trainers[idx],
+  };
+};
+
+const getAsset = await assetLoader();
+
+const generateTrainerCard = async (username: string) => {
+  const trainerData = generateTrainerData(username);
+
+  console.log("ok!");
+  const img = await jimp.read("assets/card.png");
+  img.composite(getAsset.pokemon(32), 0, 0);
+
+  await img.writeAsync("saved/card.png");
+};
+
+generateTrainerCard("xandxandxand");
--- src/generateTrainerData.ts
+++ src/generateTrainerData.ts
@@ -1,5 +1,4 @@
 import { createHash } from "crypto";
-
 const REGIONS = ["Kanto", "Johto"];
 
 const CITIES = [
@@ -25,6 +24,14 @@ const CITIES = [
   "Violet City",
 ];
 
+export const COUNT = {
+  pokemons: 252,
+  trainers: 107,
+  cities: CITIES.length,
+  regions: REGIONS.length,
+  badges: 8,
+};
+
 const sha256 = (input: string) =>
   createHash("sha-256").update(input).digest("hex");
 
@@ -35,18 +42,18 @@ export const generateTrainerData = (username: string) => {
 
   return {
     name: username,
-    region: REGIONS[parseHex(hash.substring(0, 4)) % 2],
-    hometown: CITIES[parseHex(hash.substring(0, 4)) % 20],
+    region: REGIONS[parseHex(hash.substring(0, 4)) % COUNT.regions],
+    hometown: CITIES[parseHex(hash.substring(0, 4)) % COUNT.cities],
     money: parseHex(hash.substring(5, 9)),
     pokedex: (parseHex(hash.substring(10, 14)) % 246) + 6,
-    badges: parseHex(hash.substring(15, 19)) % 9,
-    trainer: parseHex(hash.substring(20, 24)) % 107,
-    pokemon1: parseHex(hash.substring(25, 29)) % 252,
-    pokemon2: parseHex(hash.substring(30, 34)) % 252,
-    pokemon3: parseHex(hash.substring(35, 39)) % 252,
-    pokemon4: parseHex(hash.substring(40, 44)) % 252,
-    pokemon5: parseHex(hash.substring(45, 49)) % 252,
-    pokemon6: parseHex(hash.substring(50, 54)) % 252,
+    badges: (parseHex(hash.substring(15, 19)) % COUNT.badges) + 1,
+    trainer: parseHex(hash.substring(20, 24)) % COUNT.trainers,
+    pokemon1: parseHex(hash.substring(25, 29)) % COUNT.pokemons,
+    pokemon2: parseHex(hash.substring(30, 34)) % COUNT.pokemons,
+    pokemon3: parseHex(hash.substring(35, 39)) % COUNT.pokemons,
+    pokemon4: parseHex(hash.substring(40, 44)) % COUNT.pokemons,
+    pokemon5: parseHex(hash.substring(45, 49)) % COUNT.pokemons,
+    pokemon6: parseHex(hash.substring(50, 54)) % COUNT.pokemons,
     id: parseHex(hash.substring(55, 59)),
   };
 };
